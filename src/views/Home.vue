88a78e8878c44fed12a8ff7f80cef4130da5bddc
GWL0927 <gwl4111008@163.com>
2022-03-08 00:47:21 +0800

    样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
   <div id="main">
     <div>
-      <div>请按照默认值的格式输入自己的信息！</div>
+      <div>请按照提示格式填写！</div>
       <div style="margin-bottom: 2vh">使用者自己承担一切后果！</div>
       姓名: <input type="text" v-model="formData.name" /><br />
       学号: <input type="text" v-model="formData.uid" /><br />
-      手机号: <input type="text" v-model="formData.phone" /><br />
-      专业: <input type="text" v-model="formData.zy" /><br />
-      班级: <input type="text" v-model="formData.bj" /><br />
+      手机号: <input type="text" v-model="formData.phone" placeholder="133******33"/><br />
+      专业: <input type="text" v-model="formData.zy" placeholder="软件工程"/><br />
+      班级: <input type="text" v-model="formData.bj" placeholder="软件工程18A"/><br />
       班主任: <input type="text" v-model="formData.bzr" /><br />
       密钥: <input type="text" v-model="formData.secret" /><br />
-      <button @click="toAbout">进入请假条</button>
+      <div style="float: right; padding-right: 4vw"><button @click="toAbout">进入请假条</button></div>
     </div>
   </div>
 </template>
