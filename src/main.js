962aeb686d3cc627eafcc197aa2e96e90edec318
GWL0927 <gwl4111008@163.com>
2022-03-08 16:51:25 +0800

    引入vant组件

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import 'vant/lib/field/style';
+import 'vant/lib/button/style';
+import 'vant/lib/cell-group/style';
+import { Field, Button, CellGroup  } from 'vant'
+Vue.use(Field)
+Vue.use(Button);
+Vue.use(CellGroup);
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,19 @@
 <template>
   <div id="main">
-    <div>
-      <div>请按照提示格式填写！</div>
-      <div style="margin-bottom: 2vh">使用者自己承担一切后果！</div>
-      姓名: <input type="text" v-model="formData.name" /><br />
-      学号: <input type="text" v-model="formData.uid" /><br />
-      手机号: <input type="text" v-model="formData.phone" placeholder="133******33"/><br />
-      专业: <input type="text" v-model="formData.zy" placeholder="软件工程"/> <button @click="toAbout">go</button><br />
-      班级: <input type="text" v-model="formData.bj" placeholder="软件工程18A"/><br />
-      班主任: <input type="text" v-model="formData.bzr" /><br />
-      密钥: <input type="text" v-model="formData.secret" /><br />
-    </div>
+    <van-cell-group title="请按照提示格式填写，使用者自己承担一切后果！" inset>
+      <van-field v-model="formData.name" label="姓名" placeholder="请输入姓名" />
+      <van-field v-model="formData.uid" label="学号" placeholder="请输入学号" />
+      <van-field v-model="formData.phone" label="手机号" placeholder="133******33" />
+      <van-field v-model="formData.zy" label="专业" placeholder="软件工程" />
+      <van-field v-model="formData.bj" label="班级" placeholder="软件工程18A" />
+      <van-field v-model="formData.bzr" label="班主任" placeholder="请输入班主任姓名" />
+      <van-field v-model="formData.secret" type="password" label="密钥" placeholder="请输入密钥" />
+      <van-button @click="toAbout" type="info" block >进入请假条</van-button>
+    </van-cell-group>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-
 export default {
   name: "Home",
   data() {
@@ -41,9 +38,6 @@ export default {
 </script>
 <style scoped>
 #main {
-  display:flex;
-  justify-content:center;
-  align-items:center;
-  height: 70vh;  
+  margin-top: 1vh;
 }
 </style>
